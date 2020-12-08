c26f6a3c18b6febfd36a6be79c988d74001092b1
Jake <jvanboch@gmail.com>
2020-12-08 12:34:07 -0800

    updated proxy css

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var app = express()
 
-app.use(express.static(`${__dirname}/client/dist`));
+app.use(express.static(`${__dirname}/client/dist/`));
 app.get('/products/:id', function (req, res) {
   // app.use(express.static(`${__dirname}/client/dist`));
   res.sendFile(`${__dirname}/client/dist/index.html`);
